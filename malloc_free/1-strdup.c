dc742623147701b20ed2816e40579039e2a6601f
mac <mac@SK4DYs-MacBook.local>
2025-03-12 02:19:39 +0100

    malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 
-
+/**
+ * _strdup - Duplicate a string using malloc
+ * @str: string to duplicate
+ * Return: Pointer to a the new duped string
+ */
 
 char *_strdup(char *str)
 {
